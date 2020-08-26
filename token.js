3679680b98714837fb86ac23598dcfb1af426203
VCP-dev <vinaycpaul@gmail.com>
2020-08-26 18:46:51 +0530

    final touches for heroku

---
+++ token.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tokenvalue:"NzQ3NDAzNTUyMTI0Njk4NjY2.X0OXzA.USt_xNskubW7UH1L9jKeT93eVkQ",
+    roleids:{
+        mod:"747426469814206635",
+        fellow_pig:"747431864389730416",
+        bad_piggies:"747434875384103024"
+    }
+}
