93d2be5dc76ca2f136e5922aa36b3bd656b041ab
VCP-dev <vinaycpaul@gmail.com>
2020-08-26 19:10:26 +0530

    changed allcommand command

--- index.js
+++ index.js
@@ -43,28 +43,11 @@ client.on('message',(message)=>{
 
         switch(command){
             case 'allcommands':
-                let commandlist="Prefix the following with []\n\n\n"
-                let admincommands=[]
-                //let everyonecommands=[]
-                commandlist+="Commands anyone can use:\n\n"
+                let commandlist="Prefix the following with []\n\n\n"                
                 for(const file of commandFiles){
-                    let cmd = require(`./commands/${file}`)
-                    if(cmd.who_can_use==="everyone"){
-                        //everyonecommands.push(cmd)
-                        commandlist+=cmd.name+"\n"+"```"+cmd.description+"```"+"\n"
-                    }else if(cmd.who_can_use==="admin_only"){
-                        admincommands.push(cmd)
-                    }
-                    //commandlist+=(cmd.name+"\n"+"```"+cmd.description+"```"+"\n")                    
-                }    
-                //commandlist+="Commands anyone can use:\n\n"
-                /*everyonecommands.forEach((cmd)=>{
-                    commandlist+=(cmd.name+"\n"+"```"+cmd.description+"```"+"\n")
-                })*/ 
-                commandlist+="\n\nCommands only admins can use:\n\n"
-                admincommands.forEach((cmd)=>{
-                    commandlist+=(cmd.name+"\n"+"```"+cmd.description+"```"+"\n")
-                })   
+                    let cmd = require(`./commands/${file}`)                    
+                    commandlist+=(cmd.name+"\n"+"```"+cmd.description+"```"+"\n")                                      
+                }        
                 message.channel.send(simplemessageembed.embed(commandlist))
                 break; 
             case 'quack':   
