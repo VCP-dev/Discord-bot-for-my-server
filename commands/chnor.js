3d81506eef3fe7ae68369c0afdede2937fca7b7d
VCP-dev <vinaycpaul@gmail.com>
2020-08-30 11:39:42 +0530

    Added a funny message if a message has duck in it

--- commands/chnor.js
+++ commands/chnor.js
@@ -7,7 +7,7 @@ module.exports = {
     description:'Some jokes about Chuck Norris',
     async execute(message,args){
 
-       const { fetchedjoke } = await fetch('http://api.icndb.com/jokes/random').then(response => response.json()).then(fetchedjoke =>  console.log(fetchedjoke));      
+       fetch('http://api.icndb.com/jokes/random').then(response => response.json()).then(fetchedjoke =>  console.log(fetchedjoke));      
 
     }
 }
