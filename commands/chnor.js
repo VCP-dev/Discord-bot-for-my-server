ac1325312a50af850754b11942d059ec3d8c52c6
VCP-dev <vinaycpaul@gmail.com>
2020-09-23 17:48:23 +0530

    Change in URL

--- commands/chnor.js
+++ commands/chnor.js
@@ -7,9 +7,9 @@ module.exports = {
     description:'Some jokes about Chuck Norris',
     async execute(message,args){
 
-       fetch('http://api.icndb.com/jokes/random')
-            .then(response => response.json())
-            .then(fetchedjoke => message.channel.send(fetchedjoke.value.joke.replace("&quot;",'"')));      
+        fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json())
+        .then(fetchedjoke => message.channel.send(fetchedjoke.value.replace("&quot;",'"')));      
 
     }
 }
