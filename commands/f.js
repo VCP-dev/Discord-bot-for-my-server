17cca7fce6a58339ad24170cf533fd5dc5fa3836
VCP-dev <vinaycpaul@gmail.com>
2020-09-30 19:23:33 +0530

    tweaking a few commands

--- commands/f.js
+++ commands/f.js
@@ -2,7 +2,7 @@ module.exports = {
     name:'f',
     who_can_use:'everyone',
     description:'Press F to pay respect',
-    execute(message,args){
-        message.channel.send("F")
+    execute(message,args){            
+      message.channel.send("<:F_:760855700116340807>")            
     }
 }
